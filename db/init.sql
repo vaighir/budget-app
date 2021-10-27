8081de41ca91c53bfed16c2325142d36fea2f68a
Wiktor Sierpinski <vaighir@gmail.com>
2021-10-27 05:47:01 +0200

    Change database to postgresql

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,6 @@
-use test_db;
-
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) UNIQUE NOT NULL,
+  id serial PRIMARY KEY,
+  username VARCHAR(50) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
